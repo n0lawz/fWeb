b24cd25cf8fd9863792e716df20c6b2c22256de4
Nat <nlaw.dev@gmail.com>
2022-03-14 16:42:37 -0700

    created new instance of eventing in class user

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,9 @@ import { User } from "./models/Users";
 const user = new User({name: 'new record', age: 0});
 
 
-user.save();
+user.events.on('change', () => {
+    console.log('change!');
+});
+
+user.events.trigger('change');
 
