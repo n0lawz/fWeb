bbdddf3e6052911e15699dd1c7096fbb039eecc1
Nat <nlaw.dev@gmail.com>
2022-03-13 19:58:22 -0700

    extracted eventing logic from user class into eventing class

---
+++ src/models/Eventing.ts
@@ -0,0 +1,26 @@
+// used in on method to pass in argument
+type Callback = () => void;
+
+export class Eventing {
+
+    events: { [key: string]: Callback[] } = {};
+
+    on(eventName: string, callback: Callback): void {
+        const handlers = this.events[eventName] || [];
+        handlers.push(callback);
+        this.events[eventName] = handlers;
+    }
+
+    trigger(eventName: string): void {
+        const handlers = this.events[eventName];
+
+        if (!handlers || handlers.length === 0) {
+            return;
+        }
+
+        handlers.forEach(callback => {
+            callback();
+        });
+    }
+
+}
--- src/models/Users.ts
+++ src/models/Users.ts
@@ -7,12 +7,7 @@ interface UserProps {
     age?: number;
 }
 
-// used in on method to pass in argument
-type Callback = () => void;
-
 export class User {
-
-    events: { [key: string]: Callback[] } = {};
 
     constructor(private data: UserProps) {}
 
@@ -23,25 +18,7 @@ export class User {
     set(update: UserProps): void {
         Object.assign(this.data, update);
     }
-    
 
-    on(eventName: string, callback: Callback): void {
-        const handlers = this.events[eventName] || [];
-        handlers.push(callback);
-        this.events[eventName] = handlers;
-    }
-
-    trigger(eventName: string): void {
-        const handlers = this.events[eventName];
-
-        if (!handlers || handlers.length === 0) {
-            return;
-        }
-
-        handlers.forEach(callback => {
-            callback();
-        });
-    }
 
     fetch(): void {
         axios.get(`http://localhost:3000/users/${this.get('id')}`)
