82bc64b09e7ce2b29e81975f64362544828b39c3
Nat <nlaw.dev@gmail.com>
2022-03-18 13:14:31 -0700

    implemented generic constraints around get method in attrs class

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -1,8 +1,10 @@
+import { UserProps } from "./Users";
+
 export class Attributes<T> {
 
     constructor(private data: T) {}
 
-    // limiting the different types that K can be. It can only ever be one of the keys of T
+    // limiting the different types that K can be. It can only ever be one of the types of T (which is UserProps)
     get<K extends keyof T>(key: K): T[K] {
         return this.data[key];
     } 
@@ -11,3 +13,13 @@ export class Attributes<T> {
         Object.assign(this.data, update);
     }
 }
+
+const attrs = new Attributes<UserProps>({
+    id: 5,
+    age: 20,
+    name: 'yes'
+})
+
+const name = attrs.get('name');
+const age = attrs.get('age');
+const id = attrs.get('id');
